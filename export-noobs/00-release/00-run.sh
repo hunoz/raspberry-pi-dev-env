f02bdd0b1ce539b91337c04d7c7174ed31f39321
Serge Schneider <serge@raspberrypi.org>
2016-05-27 11:56:23 +0100

    Add noobs export stage

---
+++ export-noobs/00-release/00-run.sh
@@ -0,0 +1,31 @@
+***REMOVED***
+
+NOOBS_DIR="${STAGE_WORK_DIR***REMOVED***/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***"
+
+install -v -m 744	files/partition_setup.sh	${NOOBS_DIR***REMOVED***/
+install -v		files/partitions.json		${NOOBS_DIR***REMOVED***/
+install -v		files/os.json			${NOOBS_DIR***REMOVED***/
+install -v		files/Raspbian.png		${NOOBS_DIR***REMOVED***/
+install -v		files/release_notes.txt		${NOOBS_DIR***REMOVED***/
+
+tar -v -c -C		files/marketing			-f ${NOOBS_DIR***REMOVED***/marketing.tar .
+
+BOOT_SIZE=$(xz --robot -l ${NOOBS_DIR***REMOVED***/boot.tar.xz  | grep totals | cut -f 5)
+ROOT_SIZE=$(xz --robot -l ${NOOBS_DIR***REMOVED***/root.tar.xz  | grep totals | cut -f 5)
+
+BOOT_SIZE=$(expr ${BOOT_SIZE***REMOVED*** / 1000000 \+ 1)
+ROOT_SIZE=$(expr ${ROOT_SIZE***REMOVED*** / 1000000 \+ 1)
+
+BOOT_NOM=$(expr ${BOOT_SIZE***REMOVED*** \* 3)
+ROOT_NOM=$(expr ${ROOT_SIZE***REMOVED*** \+ 400)
+
+sed ${NOOBS_DIR***REMOVED***/partitions.json -i -e "s|BOOT_SIZE|${BOOT_SIZE***REMOVED***|"
+sed ${NOOBS_DIR***REMOVED***/partitions.json -i -e "s|ROOT_SIZE|${ROOT_SIZE***REMOVED***|"
+
+sed ${NOOBS_DIR***REMOVED***/partitions.json -i -e "s|BOOT_NOM|${BOOT_NOM***REMOVED***|"
+sed ${NOOBS_DIR***REMOVED***/partitions.json -i -e "s|ROOT_NOM|${ROOT_NOM***REMOVED***|"
+
+sed ${NOOBS_DIR***REMOVED***/release_notes.txt -i -e "s|UNRELEASED|${IMG_DATE***REMOVED***|"
+sed ${NOOBS_DIR***REMOVED***/os.json -i -e "s|UNRELEASED|${IMG_DATE***REMOVED***|"
+
+cp -a ${NOOBS_DIR***REMOVED*** ${DEPLOY_DIR***REMOVED***/
---
+++ export-noobs/prerun.sh
@@ -0,0 +1,25 @@
+***REMOVED***
+
+IMG_FILE="${STAGE_WORK_DIR***REMOVED***/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***.img"
+NOOBS_DIR="${STAGE_WORK_DIR***REMOVED***/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***"
+unmount_image ${IMG_FILE***REMOVED***
+
+cp ${WORK_DIR***REMOVED***/export-image/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***.img ${STAGE_WORK_DIR***REMOVED***/
+
+rm -rf ${STAGE_WORK_DIR***REMOVED***/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***
+
+LOOP_DEV=`kpartx -asv ${IMG_FILE***REMOVED*** | grep -E -o -m1 'loop[[:digit:]]+' | head -n 1`
+BOOT_DEV=/dev/mapper/${LOOP_DEV***REMOVED***p1
+ROOT_DEV=/dev/mapper/${LOOP_DEV***REMOVED***p2
+
+mkdir -p ${STAGE_WORK_DIR***REMOVED***/rootfs
+mkdir -p ${STAGE_WORK_DIR***REMOVED***/bootfs
+mkdir -p ${NOOBS_DIR***REMOVED***
+
+mount $ROOT_DEV ${STAGE_WORK_DIR***REMOVED***/rootfs
+mount $BOOT_DEV ${STAGE_WORK_DIR***REMOVED***/bootfs
+
+tar -I pxz -C ${STAGE_WORK_DIR***REMOVED***/bootfs -cpf ${NOOBS_DIR***REMOVED***/boot.tar.xz .
+tar -I pxz -C ${STAGE_WORK_DIR***REMOVED***/rootfs -cpf ${NOOBS_DIR***REMOVED***/root.tar.xz .
+
+unmount_image ${IMG_FILE***REMOVED***
