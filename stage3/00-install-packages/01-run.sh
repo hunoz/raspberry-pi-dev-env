7144fcb888bdd785680289582c695a7f0ac1197e
Serge Schneider <serge@raspberrypi.org>
2016-09-05 13:58:41 +1000

    stage3: add chromium

---
+++ stage3/00-install-packages/01-run.sh
@@ -0,0 +1,9 @@
+***REMOVED***
+
+on_chroot sh -e - <<***REMOVED***
+update-alternatives --install /usr/bin/x-www-browser \
+  x-www-browser /usr/bin/chromium-browser 86
+update-alternatives --install /usr/bin/gnome-www-browser \
+  gnome-www-browser /usr/bin/chromium-browser 86
+***REMOVED***
+
