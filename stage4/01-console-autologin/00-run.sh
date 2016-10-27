bacc547fe7e7215aac1cec797d1c4e7a2e414557
Serge Schneider <serge@raspberrypi.org>
2016-10-27 17:41:15 +0100

    stage4: remove 40-scratch.rules (added to nuscratch)

--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -1,5 +1,3 @@
 ***REMOVED***
 
 ln -sf /etc/systemd/system/autologin@.service ${ROOTFS_DIR***REMOVED***/etc/systemd/system/getty.target.wants/getty@tty1.service
-
-install -v -m 644 files/40-scratch.rules ${ROOTFS_DIR***REMOVED***/etc/udev/rules.d/
