877eaf5ad7919da5b2d53d9c46bb9625425335ec
Serge Schneider <serge@raspberrypi.org>
2016-11-24 20:07:05 +0000

    Port regenerate_ssh_host_key to a systemd service

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -1,6 +1,6 @@
 ***REMOVED***
 
-install -m 755 files/regenerate_ssh_host_keys		${ROOTFS_DIR***REMOVED***/etc/init.d/
+install -m 644 files/regenerate_ssh_host_keys.service	${ROOTFS_DIR***REMOVED***/lib/systemd/system/
 install -m 755 files/apply_noobs_os_config		${ROOTFS_DIR***REMOVED***/etc/init.d/
 install -m 755 files/resize2fs_once			${ROOTFS_DIR***REMOVED***/etc/init.d/
 
