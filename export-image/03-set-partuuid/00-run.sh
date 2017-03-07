dffd5c659835ca82e44e67769205d28766c7b8e8
Serge Schneider <serge@raspberrypi.org>
2017-03-07 15:13:22 +0000

    Switch to PARTUUID

---
+++ export-image/03-set-partuuid/00-run.sh
@@ -0,0 +1,12 @@
+***REMOVED***
+IMG_FILE="${STAGE_WORK_DIR***REMOVED***/${IMG_DATE***REMOVED***-${IMG_NAME***REMOVED***${IMG_SUFFIX***REMOVED***.img"
+
+IMGID="$(fdisk -l ${IMG_FILE***REMOVED*** | sed -n 's/Disk identifier: 0x\([^ ]*\)/\1/p')"
+
+BOOT_PARTUUID="${IMGID***REMOVED***-01"
+ROOT_PARTUUID="${IMGID***REMOVED***-02"
+
+sed -i "s/BOOTDEV/PARTUUID=${BOOT_PARTUUID***REMOVED***/" ${ROOTFS_DIR***REMOVED***/etc/fstab
+sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID***REMOVED***/" ${ROOTFS_DIR***REMOVED***/etc/fstab
+
+sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID***REMOVED***/" ${ROOTFS_DIR***REMOVED***/boot/cmdline.txt
