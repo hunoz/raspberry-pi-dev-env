027b707149b36e0ad726c7bbe50ffd8db0043ecf
Serge Schneider <serge@raspberrypi.org>
2017-07-17 14:15:48 +0100

    Switch to Stretch

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,7 +1,6 @@
 ***REMOVED***
 
 install -m 644 files/ipv6.conf ${ROOTFS_DIR***REMOVED***/etc/modprobe.d/ipv6.conf
-install -m 644 files/interfaces ${ROOTFS_DIR***REMOVED***/etc/network/interfaces
 install -m 644 files/hostname ${ROOTFS_DIR***REMOVED***/etc/hostname
 
 on_chroot << ***REMOVED***
