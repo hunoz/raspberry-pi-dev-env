adce42d609077fc7142a651a38bcb183919ecfb5
Serge Schneider <serge@raspberrypi.org>
2017-08-21 10:15:08 +0100

    Disable predictable network interface names

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,6 +3,4 @@
 install -m 644 files/ipv6.conf ${ROOTFS_DIR***REMOVED***/etc/modprobe.d/ipv6.conf
 install -m 644 files/hostname ${ROOTFS_DIR***REMOVED***/etc/hostname
 
-on_chroot << ***REMOVED***
-dpkg-divert --add --local /lib/udev/rules.d/75-persistent-net-generator.rules
-***REMOVED***
+ln -s /dev/null ${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link
