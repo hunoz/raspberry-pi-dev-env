7c7f1f02cd3b4d12e09f4e03f9480214309f6b6f
Serge Schneider <serge@raspberrypi.org>
2017-08-31 11:24:01 +0100

    stage1: fix symlink creation
    Fixes #100

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,4 +3,4 @@
 install -m 644 files/ipv6.conf ${ROOTFS_DIR***REMOVED***/etc/modprobe.d/ipv6.conf
 install -m 644 files/hostname ${ROOTFS_DIR***REMOVED***/etc/hostname
 
-ln -s /dev/null ${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link
+ln -sf /dev/null ${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link
