c0b129839e00d17132d9f5eba3335391bf80a9d3
Serge Schneider <serge@raspberrypi.org>
2018-02-20 09:46:31 +0000

    stage4: drop 'Documents' directory
    Closes #121

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -18,14 +18,6 @@ tar xvf files/python_games.tar.gz -C ${ROOTFS_DIR***REMOVED***/home/pi/python_ga
 chown 1000:1000 ${ROOTFS_DIR***REMOVED***/home/pi/python_games -Rv
 chmod +x ${ROOTFS_DIR***REMOVED***/home/pi/python_games/launcher.sh
 
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/Documents"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/Documents/BlueJ Projects"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/Documents/Greenfoot Projects"
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/Documents/Scratch Projects"
-rsync -a --chown=1000:1000 ${ROOTFS_DIR***REMOVED***/usr/share/doc/BlueJ/ "${ROOTFS_DIR***REMOVED***/home/pi/Documents/BlueJ Projects"
-rsync -a --chown=1000:1000 ${ROOTFS_DIR***REMOVED***/usr/share/doc/Greenfoot/ "${ROOTFS_DIR***REMOVED***/home/pi/Documents/Greenfoot Projects"
-rsync -a --chown=1000:1000 ${ROOTFS_DIR***REMOVED***/usr/share/scratch/Projects/Demos/ "${ROOTFS_DIR***REMOVED***/home/pi/Documents/Scratch Projects"
-
 #Alacarte fixes
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/.local"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/.local/share"
