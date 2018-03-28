b6afc5c185e99e41814415fc2fa1edbeae970be8
Serge Schneider <serge@raspberrypi.org>
2018-03-28 10:58:28 +0100

    sources.list: change {mirrordirector,archive}.raspbian.org to raspbian.raspberrypi.org

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,5 @@
 ***REMOVED***
 
 if [ ! -d "${ROOTFS_DIR***REMOVED***" ]; then
-	bootstrap stretch "${ROOTFS_DIR***REMOVED***" http://mirrordirector.raspbian.org/raspbian/
+	bootstrap stretch "${ROOTFS_DIR***REMOVED***" http://raspbian.raspberrypi.org/raspbian/
 fi
