25628e6ea660ae51955510bf7b079d81dc48ffb8
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-01-15 11:14:45 +0000

    Fixed another hard reference to the 'pi' user name

--- stage4/03-magpi/00-run.sh
+++ stage4/03-magpi/00-run.sh
@@ -11,6 +11,5 @@ fi
 
 file "files/$magpi_latest" | grep -q "PDF document"
 
-install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/pi/MagPi"
-install -v -o 1000 -g 1000 -m 644 "files/$magpi_latest" "${ROOTFS_DIR***REMOVED***/home/pi/MagPi/"
-
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR***REMOVED***/home/${FIRST_USER_NAME***REMOVED***/MagPi"
+install -v -o 1000 -g 1000 -m 644 "files/$magpi_latest" "${ROOTFS_DIR***REMOVED***/home/${FIRST_USER_NAME***REMOVED***/MagPi/"
