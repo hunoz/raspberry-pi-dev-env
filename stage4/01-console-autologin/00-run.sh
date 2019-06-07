9324cdd2fd41217f6e96be0db20d02d6a8448ee4
Serge Schneider <serge@raspberrypi.org>
2019-06-07 17:01:59 +0100

    stage4: fix autologin

--- stage4/01-console-autologin/00-run.sh
+++ stage4/01-console-autologin/00-run.sh
@@ -1,4 +1,5 @@
 ***REMOVED***
 
-ln -sf /etc/systemd/system/autologin@.service \
-	"${ROOTFS_DIR***REMOVED***/etc/systemd/system/getty.target.wants/getty@tty1.service"
+on_chroot << ***REMOVED***
+	SUDO_USER="${FIRST_USER_NAME***REMOVED***" raspi-config nonint do_boot_behaviour B4
+***REMOVED***
