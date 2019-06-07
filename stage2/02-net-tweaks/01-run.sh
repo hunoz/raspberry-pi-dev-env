8955b00b11bf7d6526f735f7902b1778d05fa9f4
Serge Schneider <serge@raspberrypi.org>
2019-06-07 16:21:03 +0100

    stage2: Disable wifi on 5GHz models

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -15,3 +15,8 @@ on_chroot <<***REMOVED***
 wpa_passphrase "${WPA_ESSID***REMOVED***" "${WPA_PASSWORD***REMOVED***" >> "/etc/wpa_supplicant/wpa_supplicant.conf"
 ***REMOVED***
 fi
+
+# Disable wifi on 5GHz models
+mkdir -p "${ROOTFS_DIR***REMOVED***/var/lib/systemd/rfkill/"
+echo 1 > "${ROOTFS_DIR***REMOVED***/var/lib/systemd/rfkill/platform-3f300000.mmc:wlan"
+echo 1 > "${ROOTFS_DIR***REMOVED***/var/lib/systemd/rfkill/platform-fe300000.mmc:wlan"
