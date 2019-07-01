d445618951c4cdbdbf13cc87ac7706a6033d6159
Ondřej Caletka <ondrej@caletka.cz>
2019-07-01 14:21:44 +0200

    Remove obsolete IPv6 turnoff (#300)
    Turning off IPv6 by aliasing `net-pf-10` to `off` does not work anymore.
    Also, turning off IPv6 on system level breaks apps depending on IPv6
    loopback or IPv6 link-local addresssing and creates issues in both
    dual-stack and IPv6-only environments.
    
    Signed-off-by: Ondřej Caletka <ondrej@caletka.cz>

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,6 +1,5 @@
 ***REMOVED***
 
-install -m 644 files/ipv6.conf "${ROOTFS_DIR***REMOVED***/etc/modprobe.d/ipv6.conf"
 install -m 644 files/hostname "${ROOTFS_DIR***REMOVED***/etc/hostname"
 
 ln -sf /dev/null "${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link"
