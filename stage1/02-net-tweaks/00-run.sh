5dd2b140b2446ab9e53815e2677c72b30a9e77d8
Serge Schneider <serge@raspberrypi.org>
2019-11-06 14:33:07 +0000

    Add hostname to /etc/hosts

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,5 +1,6 @@
 ***REMOVED***
 
 echo "${HOSTNAME***REMOVED***" > "${ROOTFS_DIR***REMOVED***/etc/hostname"
+echo "127.0.1.1		${HOSTNAME***REMOVED***" >> "${ROOTFS_DIR***REMOVED***/etc/hosts"
 
 ln -sf /dev/null "${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link"
