7689c6756166866765c0817d7eb32e5f15f85d1d
Serge Schneider <serge@raspberrypi.org>
2019-12-05 16:25:18 +0000

    Rename HOSTNAME variable to TARGET_HOSTNAME
    Bash sets the HOSTNAME variable, overriding our default

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -1,6 +1,6 @@
 ***REMOVED***
 
-echo "${HOSTNAME***REMOVED***" > "${ROOTFS_DIR***REMOVED***/etc/hostname"
-echo "127.0.1.1		${HOSTNAME***REMOVED***" >> "${ROOTFS_DIR***REMOVED***/etc/hosts"
+echo "${TARGET_HOSTNAME***REMOVED***" > "${ROOTFS_DIR***REMOVED***/etc/hostname"
+echo "127.0.1.1		${TARGET_HOSTNAME***REMOVED***" >> "${ROOTFS_DIR***REMOVED***/etc/hosts"
 
 ln -sf /dev/null "${ROOTFS_DIR***REMOVED***/etc/systemd/network/99-default.link"
