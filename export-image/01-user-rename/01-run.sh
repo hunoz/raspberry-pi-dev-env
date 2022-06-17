7e82d12ceaf7feff27882fc2d2fe8f9879b2f8a9
Romain Bazile <romain@bazile.io>
2022-06-17 16:45:08 +0200

    Create a `DISABLE_FIRST_BOOT_USER_RENAME` flag to be set in config (#618)
    Closes #614

--- export-image/01-user-rename/01-run.sh
+++ export-image/01-user-rename/01-run.sh
@@ -1,5 +1,7 @@
 ***REMOVED***
 
-on_chroot << ***REMOVED***
-	SUDO_USER="${FIRST_USER_NAME***REMOVED***" rename-user -f -s
-***REMOVED***
+if [[ "${DISABLE_FIRST_BOOT_USER_RENAME***REMOVED***" == "0" ]]; then
+	on_chroot <<- ***REMOVED***
+		SUDO_USER="${FIRST_USER_NAME***REMOVED***" rename-user -f -s
+	***REMOVED***
+fi
