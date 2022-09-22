a05e4f886d19a7837c28e466fcee9ebbed54cd7a
Serge Schneider <serge@raspberrypi.com>
2022-09-22 13:06:38 +0100

    Disable first run wizard if DISABLE_FIRST_BOOT_USER_RENAME is set

--- export-image/01-user-rename/01-run.sh
+++ export-image/01-user-rename/01-run.sh
@@ -4,4 +4,6 @@ if [[ "${DISABLE_FIRST_BOOT_USER_RENAME***REMOVED***" == "0" ]]; then
 	on_chroot <<- ***REMOVED***
 		SUDO_USER="${FIRST_USER_NAME***REMOVED***" rename-user -f -s
 	***REMOVED***
+else
+	rm -f "${ROOTFS_DIR***REMOVED***/etc/xdg/autostart/piwiz.desktop"
 fi
