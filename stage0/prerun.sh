161976f246b7722e67f775c11dbc1fcffbb1a770
Serge Schneider <serge@raspberrypi.com>
2022-11-08 11:28:18 +0000

    Updates to avoid common pitfalls when changing the RELEASE variable
    Fixes #635, fixes #645, fixes #632

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,10 @@
 ***REMOVED***
 
+if [ "$RELEASE" != "bullseye" ]; then
+	echo "WARNING: RELEASE does not match the intended option for this branch."
+	echo "         Please check the relevant README.md section."
+fi
+
 if [ ! -d "${ROOTFS_DIR***REMOVED***" ] || [ "${USE_QCOW2***REMOVED***" = "1" ]; then
 	bootstrap ${RELEASE***REMOVED*** "${ROOTFS_DIR***REMOVED***" http://raspbian.raspberrypi.org/raspbian/
 fi
