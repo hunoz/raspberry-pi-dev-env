05e0c48214331c892d7982e9210c4a0f8a50b8b4
Serge Schneider <serge@raspberrypi.com>
2023-09-07 13:40:09 +0100

    export-image: auto-remove and purge packages on dist-upgrade

--- export-image/02-set-sources/01-run.sh
+++ export-image/02-set-sources/01-run.sh
@@ -4,6 +4,6 @@ rm -f "${ROOTFS_DIR***REMOVED***/etc/apt/apt.conf.d/51cache"
 find "${ROOTFS_DIR***REMOVED***/var/lib/apt/lists/" -type f -delete
 on_chroot << ***REMOVED***
 apt-get update
-apt-get -y dist-upgrade
+apt-get -y dist-upgrade --auto-remove --purge
 apt-get clean
 ***REMOVED***
