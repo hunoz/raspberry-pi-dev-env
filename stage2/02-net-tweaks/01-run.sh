22bdc1d3d918b75f18357ff911a1306fd013ea2d
Serge Schneider <serge@raspberrypi.com>
2023-09-07 13:34:21 +0100

    Remove dhcpcd and associated options

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,30 +3,10 @@
 install -v -d					"${ROOTFS_DIR***REMOVED***/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR***REMOVED***/etc/wpa_supplicant/"
 
-on_chroot << ***REMOVED***
-	SUDO_USER="${FIRST_USER_NAME***REMOVED***" raspi-config nonint do_boot_wait 0
-	SUDO_USER="${FIRST_USER_NAME***REMOVED***" raspi-config nonint do_netconf 1
-***REMOVED***
-
 if [ -v WPA_COUNTRY ]; then
 	on_chroot <<- ***REMOVED***
 		SUDO_USER="${FIRST_USER_NAME***REMOVED***" raspi-config nonint do_wifi_country "${WPA_COUNTRY***REMOVED***"
 	***REMOVED***
-fi
-
-if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
-on_chroot <<***REMOVED***
-set -o pipefail
-wpa_passphrase "${WPA_ESSID***REMOVED***" "${WPA_PASSWORD***REMOVED***" | tee -a "/etc/wpa_supplicant/wpa_supplicant.conf"
-***REMOVED***
-elif [ -v WPA_ESSID ]; then
-cat >> "${ROOTFS_DIR***REMOVED***/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
-
-***REMOVED***
-	ssid="${WPA_ESSID***REMOVED***"
-	key_mgmt=NONE
-***REMOVED***
-EOL
 fi
 
 # Disable wifi on 5GHz models if WPA_COUNTRY is not set
--- stage3/01-tweaks/00-run.sh
+++
@@ -1,5 +0,0 @@
-***REMOVED***
-
-on_chroot << ***REMOVED***
-	SUDO_USER="${FIRST_USER_NAME***REMOVED***" raspi-config nonint do_boot_wait 1
-***REMOVED***
